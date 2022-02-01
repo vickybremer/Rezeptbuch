01c3577f2f1748598cd89544e33ac6ac33bf8bae
Isabel <sunfloweer@users.noreply.github.com>
2022-02-01 18:37:54 +0100

    Rezeptdaten-Toggle (PopUp)

--- rezeptbuch/src/components/fullRecipe/fullRecipe.js
+++ rezeptbuch/src/components/fullRecipe/fullRecipe.js
@@ -1,12 +1,12 @@
 import styles from './fullRecipe.module.css';
-import recipeData from "../recipes/Recipe-data.json"
+//import recipeData from "../recipes/Recipe-data.json"
 
-export default function FullRecipe ({closeHandler}) {
+export default function FullRecipe ({individualRecipeData, closeHandler}) {
     return (
         <div className={styles.container}>
             <div className={styles.infoBox}>
-                <h3>{recipeData.title}</h3>
-                <p>{recipeData.ingredients}</p>
+                <h3>{individualRecipeData.title}</h3>
+                <p>{individualRecipeData.ingredients}</p>
                 <button onClick={closeHandler}>x</button>
             </div>
         </div>
--- rezeptbuch/src/components/recipes/Recipes.js
+++ rezeptbuch/src/components/recipes/Recipes.js
@@ -1,28 +1,30 @@
-// import FullRecipe from "../fullRecipe/fullRecipe.js"
-// import {useState} from "react";
+import FullRecipe from "../fullRecipe/fullRecipe.js"
+import {useState} from "react";
 import recipeData from "./Recipe-data.json"
+
+
 
 export default function Recipes () {
     console.log("This is a test for Recipe Function");
 
-    // const [showFullRecipe, setShowFullRecipe] = useState(true);
+    //zeigt den State, ob Rezept gezeigt wird
+    const [showFullRecipe, setShowFullRecipe] = useState(false)
 
-    //Button OnClick Test
-    const testClick = () => {
-        console.log("hallo");
-    }
+    const toggleFullRecipe = () => {
+      setShowFullRecipe(!showFullRecipe);
+    };
 
     //Liste der Rezepttitel wird erstellt (sind Buttons)
     const recipeList = recipeData.map(recipe => 
     <li key={recipe.id}>
-        <button onClick={testClick}>{recipe.title}</button>
+        <button onClick={toggleFullRecipe}>{recipe.title}</button>
     </li>)   
 
     //Liste wird aufgerufen
     return (
         <div>
             <h3>{recipeList}</h3>
-            {/* <FullRecipe/> */}
+            {showFullRecipe && <FullRecipe individualRecipeData={recipeData} closeHandler={toggleFullRecipe}/>}
         </div>
-    );
+    )
 }
