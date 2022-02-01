959f4cd32c9c03dcf61d61f29b546ccf08f0f641
Isabel <sunfloweer@users.noreply.github.com>
2022-02-01 15:35:32 +0100

    Rezept-Data Datei erstellt
    Titel der Rezepte werden gemapped.

---
+++ rezeptbuch/src/components/recipes/Recipe-data.js
@@ -0,0 +1,14 @@
+const recipes = [{
+    "title": "Thai Curry",
+    "ingredients": ["100g Rice", "400ml Coconut Milk", "Your favourite vegetables", "30g Cashews", "1Tbsp. Curry Paste"],
+    "steps": ["1. Cook Rice", "2. Chop vegetables", "3. Put vegetable in pan", "4. Add Coconut Milk and Seasoning"],
+    "category": "dinner",
+    "date": "12.03.2022"
+},
+{
+    "title": "Mango Pineapple Sorbet",
+    "ingredients": ["365g Pineapple diced", "250g Mango diced", "200ml Water", "200g Sugar"],
+    "steps": ["1. Combine sugar, water, pineapple and mangoes in pot over medium heat", "2. Combine with syrup", "3. freeze"],
+    "category": "dessert",
+    "date": "29.06.2034"
+}]
--- rezeptbuch/src/components/recipes/Recipes.js
+++ rezeptbuch/src/components/recipes/Recipes.js
@@ -1,4 +1,5 @@
 const recipes = [{
+        "id": 0,
         "title": "Thai Curry",
         "ingredients": ["100g Rice", "400ml Coconut Milk", "Your favourite vegetables", "30g Cashews", "1Tbsp. Curry Paste"],
         "steps": ["1. Cook Rice", "2. Chop vegetables", "3. Put vegetable in pan", "4. Add Coconut Milk and Seasoning"],
@@ -6,6 +7,7 @@ const recipes = [{
         "date": "12.03.2022"
     },
     {
+        "id": 1,
         "title": "Mango Pineapple Sorbet",
         "ingredients": ["365g Pineapple diced", "250g Mango diced", "200ml Water", "200g Sugar"],
         "steps": ["1. Combine sugar, water, pineapple and mangoes in pot over medium heat", "2. Combine with syrup", "3. freeze"],
@@ -13,11 +15,18 @@ const recipes = [{
         "date": "29.06.2034"
 }]
 
-export default function Recipes ({}) {
-    console.log("this is a test for Recipe Function");
+export default function Recipes () {
+    console.log("This is a test for Recipe Function");
+
+    const theRecipe = recipes.map(singleRecipe => 
+    <li key={singleRecipe.id}>{singleRecipe.title}</li>)   
+
     return(
-    <div>
-        <h3>{recipes[0].category}</h3>
-    </div>
+        <div>
+            <ul>{theRecipe}</ul>
+        </div>
+    // <div>
+    //     <h3>{recipes[0].title}</h3>
+    // </div>
     ); 
 }
