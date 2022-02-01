5f25be7be37a77ef71241af694ac0955ab6f5820
Isabel <sunfloweer@users.noreply.github.com>
2022-02-01 16:08:47 +0100

    Recipes.js: Rezepttitel zu Buttons
    + OnClick-Test

--- rezeptbuch/src/components/recipes/Recipes.js
+++ rezeptbuch/src/components/recipes/Recipes.js
@@ -1,4 +1,4 @@
-const recipes = [{
+const recipeData = [{
         "id": 0,
         "title": "Thai Curry",
         "ingredients": ["100g Rice", "400ml Coconut Milk", "Your favourite vegetables", "30g Cashews", "1Tbsp. Curry Paste"],
@@ -13,20 +13,35 @@ const recipes = [{
         "steps": ["1. Combine sugar, water, pineapple and mangoes in pot over medium heat", "2. Combine with syrup", "3. freeze"],
         "category": "dessert",
         "date": "29.06.2034"
+    },
+    {
+        "id": 2,
+        "title": "Tzatziki",
+        "ingredients": ["400g plain yogurt", "1 cucumber", "2 gloves of garlic", "Juice of half a lemon", "Salt", "Pepper"],
+        "steps": ["1. Shred the cucumber", "2. Squeeze water out of cucumber", "3. Mix everything in a bowl"],
+        "category": "side",
+        "date": "15.07.2021"
 }]
 
 export default function Recipes () {
     console.log("This is a test for Recipe Function");
 
-    const theRecipe = recipes.map(singleRecipe => 
-    <li key={singleRecipe.id}>{singleRecipe.title}</li>)   
+    //Button OnClick Test
+    const testClick = () => {
+        console.log("hallo");
+    }
 
+    //Liste der Rezepttitel wird erstellt (sind Buttons)
+    const recipeList = recipeData.map(recipe => 
+    <li key={recipe.id}>
+        <button onClick={testClick}>{recipe.title}</button>
+    </li>)   
+
+
+    //Liste wird aufgerufen
     return(
         <div>
-            <ul>{theRecipe}</ul>
+            <h3>{recipeList}</h3>
         </div>
-    // <div>
-    //     <h3>{recipes[0].title}</h3>
-    // </div>
     ); 
 }
