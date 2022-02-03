7cf19feebb75bfec02bed33bb2195add96eebc37
vickybremer <58940112+vickybremer@users.noreply.github.com>
2022-02-03 16:15:59 +0100

    Basic Randomizer hinzugefügt

--- rezeptbuch/src/components/randomizer/Randomizer.js
+++ rezeptbuch/src/components/randomizer/Randomizer.js
@@ -1,11 +1,16 @@
 import Header from '../header/Header.js'
 
 
-export default function Randomizer (){
-    return(
+var testarray = ["1", "2", "3", "4"];
+var rand = testarray[Math.floor(Math.random() * testarray.length)];
+
+
+export default function Randomizer() {
+    return (
         <>
             <Header title="Randomizer" />
             <h3> randomizer site </h3>
+            <h3> {rand} </h3>
         </>
     );
 }
--- rezeptbuch/src/components/recipes/Recipes.js
+++ rezeptbuch/src/components/recipes/Recipes.js
@@ -1,6 +1,6 @@
 import FullRecipe from "../fullRecipe/fullRecipe.js"
 import { useState } from "react";
-import recipeData from "./Recipe-data.json"
+import recipeData from "../recipeData/Recipe-data.json"
 import Header from '../header/Header.js'
 
 
