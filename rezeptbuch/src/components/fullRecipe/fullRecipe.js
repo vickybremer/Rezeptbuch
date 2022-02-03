a8a97ed2d0e4861dd1a45abebba2b8a42fc74d68
Isabel <sunfloweer@users.noreply.github.com>
2022-02-03 17:39:49 +0100

    Fragen notiert
    - Dinge, die wir noch abklären müssen, haben wir uns notiert.

--- rezeptbuch/src/components/fullRecipe/fullRecipe.js
+++ rezeptbuch/src/components/fullRecipe/fullRecipe.js
@@ -1,27 +1,35 @@
-import styles from './fullRecipe.module.css';
+import styles from "./fullRecipe.module.css";
 //import recipeData from "../recipes/Recipe-data.json"
 
 export default function FullRecipe({ listData, selectedRecipe, closeHandler }) {
-    //die Rezepte werden nach dem Titel gefiltert (.filter = Array)
-    const recipe = listData.filter((item) => item.title === selectedRecipe)
+  //die Rezepte werden nach dem Titel gefiltert (.filter = Array)
+  const recipe = listData.filter(item => item.title === selectedRecipe);
 
-    // const test = recipe[0].ingredients.join("\n");
-    // const bla = test.split('');
+  // const test = recipe[0].ingredients.join("\n");
+  // const bla = test.split('');
 
-    // test.forEach(function (element) {
-    // });
+  // test.forEach(function (element) {
+  // });
 
-    // const test = recipe[0].ingredients
+  // const test = recipe[0].ingredients
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.infoBox}>
-                <h3>{recipe[0].title}</h3>
-                <p>{recipe[0].ingredients.join("\n")}</p>
-                <p>{recipe[0].steps}</p>
-                <p>{"created on: " + recipe[0].date}</p>
-                <button onClick={closeHandler}> close </button>
-            </div>
-        </div>
-    )
-} 
+  return (
+    <div className={styles.container}>
+      <div className={styles.infoBox}>
+        <h3>{recipe[0].title}</h3>
+        <p>{recipe[0].ingredients.join("\n")}</p>
+        <p>{recipe[0].steps}</p>
+        <p>{"created on: " + recipe[0].date}</p>
+        <button onClick={closeHandler}> close </button>
+      </div>
+    </div>
+  );
+}
+
+/* 
+
+F R A G E N:
+- Wie können Elemente eines Arrays untereinander gelistet werden?
+- (Was ist Eslintcache?)
+
+*/
