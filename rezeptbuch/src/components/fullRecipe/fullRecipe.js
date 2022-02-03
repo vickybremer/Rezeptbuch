af80992483ba500a8ef8352ae01803009bda8032
vickybremer <58940112+vickybremer@users.noreply.github.com>
2022-02-03 15:58:39 +0100

    Versuch Array zu listen
    - Elemente des Arrays sollen untereinander aufgelistet werden (hat nicht funktioniert)

--- rezeptbuch/src/components/fullRecipe/fullRecipe.js
+++ rezeptbuch/src/components/fullRecipe/fullRecipe.js
@@ -5,15 +5,23 @@ export default function FullRecipe({ listData, selectedRecipe, closeHandler }) {
     //die Rezepte werden nach dem Titel gefiltert (.filter = Array)
     const recipe = listData.filter((item) => item.title === selectedRecipe)
 
+    // const test = recipe[0].ingredients.join("\n");
+    // const bla = test.split('');
+
+    // test.forEach(function (element) {
+    // });
+
+    // const test = recipe[0].ingredients
+
     return (
         <div className={styles.container}>
             <div className={styles.infoBox}>
                 <h3>{recipe[0].title}</h3>
-                <p>{recipe[0].ingredients}</p>
+                <p>{recipe[0].ingredients.join("\n")}</p>
                 <p>{recipe[0].steps}</p>
-                <p>{"created on: "+ recipe[0].date}</p>
-                <button onClick={closeHandler}>x</button>
+                <p>{"created on: " + recipe[0].date}</p>
+                <button onClick={closeHandler}> close </button>
             </div>
         </div>
     )
-}
+} 
