0e6c20c017a5cdf02fc6b12b4cfbb8b053bd7b5d
vickybremer <58940112+vickybremer@users.noreply.github.com>
2022-02-08 15:13:08 +0100

    Dexie installiert

--- rezeptbuch/src/components/addNewRecipe/AddRecipe.js
+++ rezeptbuch/src/components/addNewRecipe/AddRecipe.js
@@ -1,7 +1,7 @@
 import Header from '../header/Header.js'
 
-export default function AddRecipe(){
-    return(
+export default function AddRecipe() {
+    return (
         <>
             <Header title="Add new Recipe" />
             <h3>add your new favourite recipe :)</h3>
