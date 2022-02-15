6da87101f3e5ec76afe983bd68804fa83e8f72f7
Isabel <sunfloweer@users.noreply.github.com>
2022-02-15 20:33:52 +0100

    Merge Conflict behoben

--- rezeptbuch/src/components/recipes/Recipes.js
+++ rezeptbuch/src/components/recipes/Recipes.js
@@ -73,7 +73,7 @@ export default function Recipes() {
           </button>
         </div>
         {/* <button onClick={() => DeleteRecipe(recipe.id)}>x</button> */}
-      </div>
+      </div >
     ));
   }
 
