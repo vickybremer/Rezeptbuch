ba25bb996dc3a805009edf879004579507bc04a4
Isabel <sunfloweer@users.noreply.github.com>
2022-02-16 15:46:36 +0100

    Probleme gelöst

--- rezeptbuch/src/components/header/Header.js
+++ rezeptbuch/src/components/header/Header.js
@@ -1,10 +1,9 @@
-import styles from "./Header.css";
+import styles from "./Header.module.css";
 
 export default function Header({ title }) {
   return (
     <div className={styles.headerStyling}>
       <h1 id="header">{title}</h1>
-
     </div>
   );
 }
